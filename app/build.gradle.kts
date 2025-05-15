735bb48433391ab887768bada21bfad028260eea
XGWNJE <xgwnje@qq.com>
2025-05-16 03:38:13 +0800

    0.8.5 更新主页

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "0.7"
+        versionName = "0.8.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
